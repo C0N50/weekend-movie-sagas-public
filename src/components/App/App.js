58e4e836c732f0dabf43a9e83e639d6ea866c848
Connor Ferris <ferr0373@gmail.com>
2023-03-04 17:31:43 -0600

    added header component and made -Conflix- title a button to bring user to home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,26 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import Details from '../Details/Details';
+import Header from '../Header/Header';
+import { useHistory } from "react-router-dom";
 
 function App() {
+
+  const history = useHistory();
+
   return (
     <div className="App">
-      <h1 className="title">CONFLIX</h1>
+
       <Router>
+
         <Route path="/" exact>
+          <Header />
           <MovieList />
         </Route>
 
         <Route path="/details" exact>
-          <Details/>
+          <Header />
+          <Details />
         </Route>
 
         {/* Add Movie page */}
